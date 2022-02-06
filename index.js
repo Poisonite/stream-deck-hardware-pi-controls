6753497e9dee799ad4015ec97b97de28c8967e2b
Zac <poisonite@gmail.com>
2022-02-05 23:33:10 -0700

    Server test

--- index.js
+++ index.js
@@ -1,34 +1,47 @@
-// const http = require("http");
-// const url = require("url");
+// Import HTTP server packages
+const http = require("http");
+const url = require("url");
 
-// http
-//   .createServer(function (req, res) {
-//     const queryObject = url.parse(req.url, true).query;
-//     console.log(queryObject);
+// Import Rasp Pi GPIO control package
+var Gpio = require("onoff").Gpio;
 
-//     res.writeHead(200, { "Content-Type": "text/html" });
-//     res.end("Feel free to add query parameters to the end of the url");
-//   })
-//   .listen(8080);
+http
+  .createServer(function (req, res) {
+    const queryObject = url.parse(req.url, true).query;
+    console.log(queryObject);
 
-var Gpio = require("onoff").Gpio; //include onoff to interact with the GPIO
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("Feel free to add query parameters to the end of the url");
+  })
+  .listen(8080);
 
-const myGPIO = 17;
+// const myGPIO = 17;
 
-var light = new Gpio(myGPIO, "out"); //use the GPIO pin we specified, and specify that it is output
+// // Reset each button to it's non-pressed value
+// const buttonGPIOs = [17, 18, 23];
 
-const pressButton = (GPIO) => {
-  // Reset button to default value (not pressed)
-  console.log(`Resetting GPIO ${myGPIO} to HIGH`);
-  //   light.writeSync(1);
-  console.log(`Setting GPIO ${myGPIO} to LOW`);
-  light.writeSync(0);
+// buttonGPIOs.forEach((GPIO) => {
+//   //use the GPIO that we specified, and specify that it is output
+//   var button = new Gpio(GPIO, "out");
 
-  // Press the button
-  setTimeout(() => {
-    console.log(`Setting GPIO ${myGPIO} to HIGH`);
-    light.writeSync(1);
-  }, 100);
-};
+//   // Reset button to default value (not pressed)
+//   console.log(`Resetting GPIO ${GPIO} to HIGH`);
+//   button.writeSync(1);
+// });
 
-pressButton(myGPIO);
+// // Do a momentary press of the provided button
+// const pressButton = (GPIO) => {
+//   //use the GPIO that we specified, and specify that it is output
+//   var button = new Gpio(GPIO, "out");
+
+//   console.log(`Setting GPIO ${GPIO} to LOW`);
+//   button.writeSync(0);
+
+//   // Press the button
+//   setTimeout(() => {
+//     console.log(`Setting GPIO ${GPIO} to HIGH`);
+//     button.writeSync(1);
+//   }, 100);
+// };
+
+// pressButton(myGPIO);
