ec41604dc35425c2ed48bcbfd7b84e761b04c6a9
Zac <poisonite@gmail.com>
2022-02-05 23:08:56 -0700

    Test 3

--- index.js
+++ index.js
@@ -17,23 +17,23 @@ const myGPIO = 18;
 
 var light = new Gpio(myGPIO, "out"); //use the GPIO pin we specified, and specify that it is output
 
-for (let i = 0; i < 2; i++) {
-  console.log(`Loop ${i}`);
+// for (let i = 0; i < 2; i++) {
+//   console.log(`Loop ${i}`);
 
-  // Reset button to default value (not pressed)
-  console.log(`Resetting GPIO ${myGPIO} to HIGH`);
+//   // Wait before starting next loop
+//   setTimeout(() => {
+//     console.log(`Waiting 300ms before starting next loop`);
+//   }, 300);
+// }
+
+// Reset button to default value (not pressed)
+console.log(`Resetting GPIO ${myGPIO} to HIGH`);
+light.writeSync(1);
+console.log(`Setting GPIO ${myGPIO} to LOW`);
+light.writeSync(0);
+
+// Press the button
+setTimeout(() => {
+  console.log(`Setting GPIO ${myGPIO} to HIGH`);
   light.writeSync(1);
-  console.log(`Setting GPIO ${myGPIO} to LOW`);
-  light.writeSync(0);
-
-  // Press the button
-  setTimeout(() => {
-    console.log(`Setting GPIO ${myGPIO} to HIGH`);
-    light.writeSync(1);
-  }, 100);
-
-  // Wait before starting next loop
-  setTimeout(() => {
-    console.log(`Waiting 300ms before starting next loop`);
-  }, 300);
-}
+}, 100);
