1d9976979f1066c181f58929379204c6cc7a6bad
Zac <poisonite@gmail.com>
2022-02-05 23:46:30 -0700

    Full test 1

--- index.js
+++ index.js
@@ -5,43 +5,73 @@ const url = require("url");
 // Import Rasp Pi GPIO control package
 var Gpio = require("onoff").Gpio;
 
+// Store a dictionary of which button is on which GPIO
+const buttonGPIOs = {
+  power: 17,
+  bright: 18,
+  dim: 23,
+};
+
+// Reset each button to it's non-pressed value
+Object.values(buttonGPIOs).forEach((GPIO) => {
+  //use the GPIO that we specified, and specify that it is output
+  var button = new Gpio(GPIO, "out");
+
+  // Reset button to default value (not pressed)
+  console.log(`Resetting GPIO ${GPIO} to HIGH`);
+  button.writeSync(1);
+});
+
+// Do a momentary press of the provided button
+const pressButton = (GPIO) => {
+  //use the GPIO that we specified, and specify that it is output
+  var button = new Gpio(GPIO, "out");
+
+  console.log(`Setting GPIO ${GPIO} to LOW`);
+  button.writeSync(0);
+
+  // Press the button
+  setTimeout(() => {
+    console.log(`Setting GPIO ${GPIO} to HIGH`);
+    button.writeSync(1);
+  }, 100);
+};
+
 http
   .createServer(function (req, res) {
     const queryObject = url.parse(req.url, true).query;
     console.log(queryObject);
+    // Turn the power on
+    if (queryObject.action === "togglePower") {
+      console.log(`Toggling power, GPIO: ${buttonGPIOs.power}`);
+      pressButton(buttonGPIOs.power);
+    }
+    // Increase brightness by 1 controller step
+    if (queryObject.action === "brightOneStep") {
+      console.log(`Raise brightness by 1 step, GPIO: ${buttonGPIOs.bright}`);
+      pressButton(buttonGPIOs.bright);
+    }
+    // Decrease brightness by 1 controller step
+    if (queryObject.action === "dimOneStep") {
+      console.log(`Lower brightness by 1 step, GPIO: ${buttonGPIOs.dim}`);
+      pressButton(buttonGPIOs.dim);
+    }
+    // Hit brightness button 20 times to simulate full brightness
+    if (queryObject.action === "fullBright") {
+      console.log(`Setting as bright as possible, GPIO: ${buttonGPIOs.bright}`);
+      for (let i = 0; i < 20; i++) {
+        pressButton(buttonGPIOs.bright);
+      }
+    }
+    // Hit dimmer button 20 times to simulate lowest brightness
+    if (queryObject.action === "fullDim") {
+      console.log(`Setting as dim as possible, GPIO: ${buttonGPIOs.dim}`);
+      for (let i = 0; i < 20; i++) {
+        pressButton(buttonGPIOs.dim);
+      }
+    }
 
     res.writeHead(200, { "Content-Type": "text/html" });
     res.end("Feel free to add query parameters to the end of the url");
   })
   .listen(8080);
-
-// const myGPIO = 17;
-
-// // Reset each button to it's non-pressed value
-// const buttonGPIOs = [17, 18, 23];
-
-// buttonGPIOs.forEach((GPIO) => {
-//   //use the GPIO that we specified, and specify that it is output
-//   var button = new Gpio(GPIO, "out");
-
-//   // Reset button to default value (not pressed)
-//   console.log(`Resetting GPIO ${GPIO} to HIGH`);
-//   button.writeSync(1);
-// });
-
-// // Do a momentary press of the provided button
-// const pressButton = (GPIO) => {
-//   //use the GPIO that we specified, and specify that it is output
-//   var button = new Gpio(GPIO, "out");
-
-//   console.log(`Setting GPIO ${GPIO} to LOW`);
-//   button.writeSync(0);
-
-//   // Press the button
-//   setTimeout(() => {
-//     console.log(`Setting GPIO ${GPIO} to HIGH`);
-//     button.writeSync(1);
-//   }, 100);
-// };
-
-// pressButton(myGPIO);
