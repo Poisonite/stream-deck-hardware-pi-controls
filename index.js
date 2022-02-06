8cd4588c7b5a47afe9593b1d963c667e8765ff6b
Zac <poisonite@gmail.com>
2022-02-05 23:07:25 -0700

    GPIO dim test 2

--- index.js
+++ index.js
@@ -13,16 +13,27 @@
 
 var Gpio = require("onoff").Gpio; //include onoff to interact with the GPIO
 
-const myGPIO = 4;
+const myGPIO = 18;
 
 var light = new Gpio(myGPIO, "out"); //use the GPIO pin we specified, and specify that it is output
 
-console.log(`Resetting GPIO ${myGPIO} to HIGH`);
-light.writeSync(1);
-console.log(`Setting GPIO ${myGPIO} to LOW`);
-light.writeSync(0);
+for (let i = 0; i < 2; i++) {
+  console.log(`Loop ${i}`);
 
-setTimeout(() => {
-  console.log(`Setting GPIO ${myGPIO} to HIGH`);
+  // Reset button to default value (not pressed)
+  console.log(`Resetting GPIO ${myGPIO} to HIGH`);
   light.writeSync(1);
-}, 100);
+  console.log(`Setting GPIO ${myGPIO} to LOW`);
+  light.writeSync(0);
+
+  // Press the button
+  setTimeout(() => {
+    console.log(`Setting GPIO ${myGPIO} to HIGH`);
+    light.writeSync(1);
+  }, 100);
+
+  // Wait before starting next loop
+  setTimeout(() => {
+    console.log(`Waiting 300ms before starting next loop`);
+  }, 300);
+}
